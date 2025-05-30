a0bc3f7bcdc4ff3a370105ddcf0bef5eaa037a95
bmincof <bminlvcoffee@gmail.com>
2025-05-30 22:22:22 +0900

    타입 예시 추가

--- example/basic/type.go
+++ example/basic/type.go
@@ -9,7 +9,7 @@ func PublicFunc() {
 	// 변수 선언
 	var x = 1
 	y := 2
-
+	// 1 2
 	fmt.Println(x, y)
 
 	// 상수 선언
@@ -17,6 +17,26 @@ func PublicFunc() {
 
 	// 배열 선언
 	arr := [3]int{1, 2, 3}
-
+	// 41 2
 	fmt.Println(constant, arr[1])
+
+	// <key: string / value: int> 맵 선언
+	// {"a": 1, "b": 2}로 초기화
+	mapExample := map[string]int{"a": 1, "b": 2}
+
+	// 0 -> 키가 없을 시 자료형의 기본값
+	fmt.Println(mapExample["A"])
+	// 2
+	fmt.Println(mapExample["b"])
+
+	// User 구조체 선언
+	type User struct {
+		Name string
+		Age  int
+	}
+
+	user := User{Name: "이름", Age: 20}
+
+	// {이름 20}
+	fmt.Println(user)
 }
